6c7ea393f5cbd8d7a333be991b76e8d78eabdae8
Rudra Inamdar <rudrainamdar293@gmail.com>
2026-06-17 10:54:10 +0530

    modify

--- conditional.cpp
+++ conditional.cpp
@@ -1,32 +1,32 @@
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int a ;
-//     cout<<"entre your 1st  no.  = ";
-//     cin>>a;
+#include <iostream>
+using namespace std;
+int main(){
+    int a ;
+    cout<<"entre your 1st  no.  = ";
+    cin>>a;
 
-//     int b ;
-//     cout<<"entre your 2nd no.  = ";
-//     cin>>b;
+    int b ;
+    cout<<"entre your 2nd no.  = ";
+    cin>>b;
     
-//     int c ;
-//     cout<<"entre your 3rd no.  = ";
-//     cin>>c;
+    int c ;
+    cout<<"entre your 3rd no.  = ";
+    cin>>c;
 
-//     if(a>b && a>c){
+    if(a>b && a>c){
 
-//         cout<<" the greatest no. is "<<a;
+        cout<<" the greatest no. is "<<a;
 
-//     }else if (b>c && b>a){
+    }else if (b>c && b>a){
 
-//         cout<<"the greatest no. is "<<b;
+        cout<<"the greatest no. is "<<b;
 
 
-//     }else{
-//         cout<<"the greatest no. is" <<c;
-//     }
-//     return 0; 
-// }
+    }else{
+        cout<<"the greatest no. is" <<c;
+    }
+    return 0; 
+}
 
 
 #include <iostream>
